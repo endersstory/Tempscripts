3308f1b595ba9cd5e84144f2bece47c54300f1f2
endersstory <43961894+endersstory@users.noreply.github.com>
2023-02-03 13:54:50 -0500

    Update ZTI.ps1

--- ZTI.ps1
+++ ZTI.ps1
@@ -4,13 +4,13 @@ Start-Sleep -Seconds 5
 
 #Change Display Resolution for Virtual Machine
 
-if ((Get-MyComputerModel) -match 'Virtual') {
+<#if ((Get-MyComputerModel) -match "Virtual") {
 
 Write-Host -ForegroundColor Green “Setting Display Resolution to 1600x”
 
 Set-DisRes 1600
 
-}
+}#>
 
 #Make sure I have the latest OSD Content
 
